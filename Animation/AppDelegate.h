a8fb4bbc3854cbc08638f990ce3cea81966b9412
yumumu <webmanyu@gmail.com>
2017-04-12 16:34:41 +0800

    初始化

---
+++ Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animation
+//
+//  Created by why001 on 12/04/2017.
+//  Copyright © 2017 why001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Animation
+//
+//  Created by why001 on 12/04/2017.
+//  Copyright © 2017 why001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
