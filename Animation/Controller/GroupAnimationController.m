fdd6ce9d46eb7695ba83a47f3b297505304d36bc
yumumu <webmanyu@gmail.com>
2017-04-13 09:31:39 +0800

    add s simple animation to tabbar switch

--- Animation/Controller/GroupAnimationController.m
+++ Animation/Controller/GroupAnimationController.m
@@ -7,6 +7,7 @@
 //
 
 #import "GroupAnimationController.h"
+#import "Masonry.h"
 
 @interface GroupAnimationController ()
 
@@ -17,21 +18,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor grayColor];
+    
+    UIScrollView *scrollView = [UIScrollView new];
+    scrollView.backgroundColor = [[UIColor purpleColor] colorWithAlphaComponent:0.4f];
+    [self.view addSubview:scrollView];
+    [scrollView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+        make.bottom.mas_equalTo(self.view).offset(200);
+    }];
+    
+    self.navigationController.hidesBarsOnSwipe = YES;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- Animation/Controller/MMTabBarController.m
+++ Animation/Controller/MMTabBarController.m
@@ -8,7 +8,7 @@
 
 #import "MMTabBarController.h"
 
-@interface MMTabBarController ()
+@interface MMTabBarController () <UITabBarControllerDelegate>
 
 @end
 
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +25,11 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController {
+    CATransition *transition = [CATransition animation];
+//    transition.duration = 0.3f;
+    transition.type = kCATransitionFade;
+    [tabBarController.view.layer addAnimation:transition forKey:nil];
 }
-*/
 
 @end
